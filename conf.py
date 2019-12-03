e7a82d9d8e35a4e61b9874e1babf0611587cd9ba
Jason Belk <jason.belk@networktocode.com>
2019-12-03 11:08:10 -0800

    fix contents.rst error

--- conf.py
+++ conf.py
@@ -23,7 +23,8 @@ author = 'Tail-F'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
-
+#fix readthedoc error not finding contents.rst
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
